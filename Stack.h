a093a2205eb9a3bce040567980b69d72c9404d71
hitode221 <marina.dr98@mail.ru>
2016-09-09 17:41:59 +0300

    Add files via upload

---
+++ Stack.h
@@ -0,0 +1,45 @@
+#pragma once
+
+template <typename T>
+class stack {
+public:
+	stack() : array_size_(0), count_(0), array_(nullptr){}
+	size_t count() const {
+		return count_;
+	}
+	size_t size() const {
+		return array_size_;
+	}
+	void push(T const & element) {
+		count_++;
+		if (array_size_ < count_) {
+			array_size_++;
+			T * temp = new T[count_];
+			for (size_t i = 0; i < count_ - 1; i++) {
+				temp[i] = array_[i];
+			}
+			temp[count_ - 1] = element;
+			delete[] array_;
+			array_ = new T[count_];
+			for (size_t i = 0; i < count_; i++) {
+				array_[i] = temp[i];
+			}
+			delete[] temp;
+		}
+		else {
+			array_[count_-1] = element;
+		}
+	}
+	T pop() {
+		if (count_ != 0) {
+			return array_[--count_];
+		}
+	}
+	~stack() {
+		if (array_size_ != 0) delete[] array_;
+	}
+private:
+	T * array_;
+	size_t array_size_;
+	size_t count_;
+};
