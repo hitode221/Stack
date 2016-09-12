1fa42e280443d7efdaaa4fd775c1a0cdbbd9123f
hitode221 <marina.dr98@mail.ru>
2016-09-12 10:20:20 +0300

    Update init.cpp

--- tests/source/init.cpp
+++ tests/source/init.cpp
@@ -3,12 +3,15 @@
 #include <iostream>
 #include <fstream>
 
+SCENARIO("Stack init, "[init]"){
+	stack<int> Stack;
+	REQUIRE(Stack.count() == 0);
+}
 SCENARIO("Stack push", "[push]") {
 	stack<int> Stack;
 	Stack.push(3);
 	REQUIRE(Stack.pop() == 3);
 }
-
 SCENARIO("Stack pop", "[pop]") {
 	stack<int> Stack;
 	for (int i = 0; i < 10; i++) {
