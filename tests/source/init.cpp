73635405c75b7c20472f17ec57dbd28ac8937285
hitode221 <marina.dr98@mail.ru>
2016-09-12 09:05:52 +0300

    Create init.cpp

---
+++ tests/source/init.cpp
@@ -0,0 +1,26 @@
+#include "Stack.h"
+#include <iostream>
+#include <fstream>
+
+SCENARIO("Stack push", "[push]") {
+	stack<int> Stack;
+	Stack.push(3);
+	REQUIRE(Stack.pop() == 3);
+}
+
+SCENARIO("Stack pop") {
+	stack<int> Stack;
+	for (int i = 0; i < 10; i++) {
+		Stack.push(i);
+	}
+	Stack.pop();
+	REQUIRE(Stack.count() == 9);
+}
+
+SCENARIO("Stack count", "[count]"){
+	stack<int> Stack;
+	for (int i = 0; i < 10; i++) {
+		Stack.push(i);
+	}
+	REQUIRE(Stack.count() == 10);
+}
