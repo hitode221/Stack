a3bd5f7bb4a5dd4e9ff2ef5b8f5b5d529b181cb2
hitode221 <marina.dr98@mail.ru>
2016-09-12 09:34:29 +0300

    Update Stack.h

--- include/Stack.h
+++ include/Stack.h
@@ -6,53 +6,20 @@ using namespace std;
 template <typename T>
 class stack {
 public:
-	stack() : array_size_(0), count_(0), array_(nullptr){}
-	stack(const stack<T> & stack_) : array_size_(stack_.array_size_), count_(stack_.count_) {
-		array_ = new T[array_size_];
-		for (size_t i = 0; i < count_; i++) {
-			array_[i] = stack_.array_[i];
-		}
-	}
-	stack<T> & operator =(stack<T> & stack_) {
-		if (this != &stack_) {
-			stack(stack_).swap(*this);
-		}
-		return *this;
-	}
-	size_t count() const {
-		return count_;
-	}
-	size_t size() const {
-		return array_size_;
-	}
-	void push(T const & element) {
-		if (array_size_ == count_) {
-			array_size_++;
-			T * temp = new T[array_size_];
-			for (size_t i = 0; i < count_; i++) {
-				temp[i] = array_[i];
-			}
-			delete[] array_;
-			array_ = temp;
-		}
-		array_[count_++] = element;
-	}
-	T pop() {
-		if (count_ != 0) {
-			return array_[--count_];
-		}
-		else throw logic_error("Empty stack");
-	}
-	void swap(stack & stack_) {
-		std::swap(array_size_, stack_.array_size_);
-		std::swap(count_, stack_.count_);
-		std::swap(array_, stack_.array_);
-	}
-	~stack() {
-		delete[] array_;
-	}
+	stack();
+	stack(const stack<T> & stack_);
+	stack<T> & operator =(stack<T> & stack_);
+	size_t count() const;
+	size_t size() const;
+	void push(T const & element);
+	T pop();
+	void swap(stack & stack_);
+	~stack();
 private:
 	T * array_;
 	size_t array_size_;
 	size_t count_;
 };
+
+#include "stack.cpp"
+
