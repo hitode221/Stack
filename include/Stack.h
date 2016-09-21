81a26e4c946df2c819159242aca3ed2c2bc032c3
hitode221 <marina.dr98@mail.ru>
2016-09-21 22:06:34 +0300

    Update Stack.h

--- include/Stack.h
+++ include/Stack.h
@@ -4,21 +4,21 @@
 using namespace std; 
 
 template <typename T>
-T* copy_array(T * array_, size_t size, size_t new_size);
+T* copy_array(T * array_, size_t size, size_t new_size); //strong
 
 template <typename T>
 class stack {
 public:
-	stack(); 
-	stack(const stack<T> & stack_); 
-	stack<T> & operator =(stack<T> & stack_);
-	size_t count() const noexcept; 
-	size_t size() const noexcept; 
-	void push(T const & element);
-	T& top() const throw(logic_error); 
-	void pop() throw(logic_error); 
-	void swap(stack & stack_) noexcept;
-	~stack(); 
+	stack(); //strong
+	stack(const stack<T> & stack_); //strong
+	stack<T> & operator =(stack<T> & stack_); //strong 
+	size_t count() const noexcept; //noexcept
+	size_t size() const noexcept; //noexcept
+	void push(T const & element); //strong
+	T& top() const throw(logic_error); //strong
+	void pop() throw(logic_error); //strong
+	void swap(stack & stack_) noexcept; //noexcept
+	~stack(); //noexcept
 private:
 	T * array_;
 	size_t array_size_;
