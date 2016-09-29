9f34744f09264f2265cdcb951513b925adff268a
hitode221 <marina.dr98@mail.ru>
2016-09-29 18:43:32 +0300

    Update stack.cpp

--- include/stack.cpp
+++ include/stack.cpp
@@ -6,30 +6,30 @@
 using namespace std;
 
 template <typename T>
-stack<T>::stack() : array_size_(0), count_(0), array_(nullptr) {}
+stack<T>::stack() : array_size_(0), count_(0), array_(nullptr) {} //noexcept
 
 template <typename T>
-stack<T>::stack(const stack<T> & stack_) : array_size_(stack_.array_size_), count_(stack_.count_) {
+stack<T>::stack(const stack<T> & stack_) : array_size_(stack_.array_size_), count_(stack_.count_) { //strong
 	array_ = copy_array(stack_.array_, array_size_, array_size_);
 }
 
 template <typename T>
-stack<T> & stack<T>::operator =(stack<T> & stack_) {
+stack<T> & stack<T>::operator =(stack<T> & stack_) {//strong
 	if (this != &stack_) {
 		stack(stack_).swap(*this);
 	}
 	return *this;
 }
 template <typename T>
-size_t stack<T>::count() const noexcept {
+size_t stack<T>::count() const noexcept {//noexcept
 	return count_;
 }
 template <typename T>
-size_t stack<T>::size() const noexcept {
+size_t stack<T>::size() const noexcept {//noexcept
 	return array_size_;
 }
 template <typename T>
-void stack<T>::push(T const & element) {
+void stack<T>::push(T const & element) {//strong
 	if (array_size_ == count_) {
 		array_size_ = array_size_ * 2 + (array_size_ == 0);
 		T * temp = copy_array(array_, count_, array_size_);
@@ -40,31 +40,31 @@ void stack<T>::push(T const & element) {
 	count_++;
 }
 template <typename T>
-T& stack<T>::top() const throw(logic_error) {
+T& stack<T>::top() const throw(logic_error) {//strong
 	if (count_ != 0) {
 		return array_[count_- 1];
 	}
 	else throw logic_error("Empty stack");
 }
 template <typename T>
-void stack<T>::pop() throw(logic_error){
+void stack<T>::pop() throw(logic_error){//strong
 	if (count_ != 0) {
 		count_--;
 	}
 	else throw logic_error("Empty stack");
 }
 template <typename T>
-void stack<T>::swap(stack & stack_) noexcept {
+void stack<T>::swap(stack & stack_) noexcept {//noexcept
 	std::swap(array_size_, stack_.array_size_);
 	std::swap(count_, stack_.count_);
 	std::swap(array_, stack_.array_);
 }
 template <typename T>
-stack<T>::~stack() {
+stack<T>::~stack() {//noexcept
 	delete[] array_;
 }
 template <typename T>
-T* copy_array(T * array_, size_t size, size_t new_size) { 
+T* copy_array(T * array_, size_t size, size_t new_size) { //strong
 	T * temp = new T[new_size]; 
 	try {
 		copy(array_, array_ + size, temp);
