055c435171000db525f5a99fff2963ce6957d915
hitode221 <marina.dr98@mail.ru>
2016-10-07 10:17:24 +0300

    Update stack.cpp

--- include/stack.cpp
+++ include/stack.cpp
@@ -1,35 +1,61 @@
-#include "Stack.h"
-#include <iostream>
+#include "stack.h"
 #ifndef STC_CPP
 #define STC_CPP
 
-using namespace std;
-
 template <typename T>
-stack<T>::stack() : array_size_(0), count_(0), array_(nullptr) {} //noexcept
-
-template <typename T>
-stack<T>::stack(const stack<T> & stack_) : array_size_(stack_.array_size_), count_(stack_.count_) { //strong
-	array_ = copy_array(stack_.array_, array_size_, array_size_);
+T* copy_array(T * array_, size_t size, size_t new_size) { //strong
+	T * temp = new T[new_size];
+	try {
+		copy(array_, array_ + size, temp);
+	}
+	catch (exception &e) {
+		delete[] temp;
+		throw;
+	}
+	return temp;
 }
 
 template <typename T>
-stack<T> & stack<T>::operator =(stack<T> & stack_) {//strong
+::allocator <T>::allocator(size_t size = 0) : array_size_(size), count_(0), array_(operator new (size*sizeof(T))) {}; //noexcept
+
+template<typename T>
+::allocator<T>::~allocator(){ //noexcept
+	delete[] array_;
+}
+
+template<typename T>
+auto ::allocator<T>::swap(allocator & other) noexcept -> void{ //noexcept
+	std::swap(array_, other.array_);
+	std::swap(count_, other.count_);
+	std::swap(array_size_, other.array_size_);
+};
+
+template <typename T> 
+stack<T>::stack() : ::allocator<T>() {};
+
+template <typename T>
+stack<T>::stack(stack const & stack_) : ::allocator<T>(stack_.count_) {
+	ptr_ = copy_array(stack_.ptr_, stack_.count_, stack_.size_);
+	size_ = stack_.size_;
+}
+
+template <typename T>
+auto stack<T> :: operator =(stack<T> & stack_)->stack<T> &{ //strong 
 	if (this != &stack_) {
 		stack(stack_).swap(*this);
 	}
 	return *this;
 }
 template <typename T>
-size_t stack<T>::count() const noexcept {//noexcept
+auto stack<T>::count() const  noexcept -> size_t{//noexcept
 	return count_;
 }
 template <typename T>
-size_t stack<T>::size() const noexcept {//noexcept
+auto stack<T>::size() const noexcept -> size_t{//noexcept
 	return array_size_;
 }
 template <typename T>
-void stack<T>::push(T const & element) {//strong
+auto stack<T>::push(T const & element) -> void{//strong
 	if (array_size_ == count_) {
 		size_t size = array_size_ * 2 + (array_size_ == 0);
 		T * temp = copy_array(array_, count_, size);
@@ -41,43 +67,22 @@ void stack<T>::push(T const & element) {//strong
 	++count_;
 }
 template <typename T>
-T& stack<T>::top() const throw(logic_error) {//strong
+auto stack<T>::top() const throw(logic_error) -> T& {//strong
 	if (count_ != 0) {
-		return array_[count_- 1];
+		return array_[count_ - 1];
 	}
 	else throw logic_error("Empty stack");
 }
 template <typename T>
-void stack<T>::pop() throw(logic_error){//strong
+auto stack<T>::pop() throw(logic_error)  -> void{//strong
 	if (count_ != 0) {
 		--count_;
 	}
 	else throw logic_error("Empty stack");
 }
 template <typename T>
-void stack<T>::swap(stack & stack_) noexcept {//noexcept
-	std::swap(array_size_, stack_.array_size_);
-	std::swap(count_, stack_.count_);
-	std::swap(array_, stack_.array_);
-}
-template <typename T>
-bool stack<T>::empty() const noexcept{ //noexcept
+auto stack<T>::empty() const noexcept -> bool{ //noexcept
 	return (count_ == 0);
 }
-template <typename T>
-stack<T>::~stack() {//noexcept
-	delete[] array_;
-}
-template <typename T>
-T* copy_array(T * array_, size_t size, size_t new_size) { //strong
-	T * temp = new T[new_size]; 
-	try {
-		copy(array_, array_ + size, temp);
-	}
-	catch (exception &e) {
-		delete[] temp;
-		throw;
-	}
-	return temp;
-}
+
 #endif
