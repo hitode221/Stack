b4a5728f7c69001a5f692a2e7b90adb7db54cfc6
hitode221 <marina.dr98@mail.ru>
2016-10-07 10:16:03 +0300

    Update Stack.h

--- include/Stack.h
+++ include/Stack.h
@@ -1,30 +1,37 @@
 #pragma once
 #include <stdexcept>
 
-using namespace std; 
-
+using namespace std;
 template <typename T>
 T* copy_array(T * array_, size_t size, size_t new_size); //strong
 
 template <typename T>
-class stack {
-public:
-	stack(); //noexcept
-	stack(const stack<T> & stack_); //strong
-	stack<T> & operator =(stack<T> & stack_); //strong 
-	size_t count() const noexcept; //noexcept
-	size_t size() const noexcept; //noexcept
-	void push(T const & element); //strong
-	bool empty() const noexcept; //noexcept
-	T& top() const throw(logic_error); //strong
-	void pop() throw(logic_error); //strong
-	void swap(stack & stack_) noexcept; //noexcept
-	~stack(); //noexcept
-private:
+class allocator {
+protected:
+	allocator(size_t size = 0); //noexcept
+	~allocator(); //noexcept
+	auto swap(allocator & other) noexcept -> void; //noexcept
+
+	allocator(allocator const &) = delete;
+	auto operator =(allocator const &)->allocator & = delete;
+
 	T * array_;
 	size_t array_size_;
 	size_t count_;
 };
 
-#include "stack.cpp"
+template <typename T>
+class stack : protected ::allocator<T> {
+public:
+	stack(); //noexcept	
+	stack(const stack<T> & stack_); //strong
+	auto operator =(stack<T> & stack_) -> stack<T> &; //strong 
+	auto count() const noexcept-> size_t; //noexcept
+	auto size() const noexcept-> size_t; //noexcept
+	auto push(T const & element) -> void; //strong
+	auto empty() const noexcept -> bool; //noexcept
+	auto top()  const throw(logic_error) -> T&; //strong
+	auto pop()  throw(logic_error) -> void; //strong
+};
 
+#include "stack.cpp"
